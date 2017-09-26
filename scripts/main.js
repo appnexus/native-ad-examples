e630836c1cff1e0f2fcc11df2bde6b034712f496
Sean Laude <slaude@appnexus.com>
2017-09-26 14:52:21 -0400

    CR-3768: refactoring styles to inline

--- scripts/main.js
+++ scripts/main.js
@@ -14,17 +14,40 @@ const indexTemplate = handlebars.compile(indexFile);
 
 const adGallery = require('../src/ad-gallery.js');
 const sampleAd = require('../src/sample-ad.js');
+const style = require('../src/style.js');
 
+/**
+ * This returns formatted html as a safestrng so that it will be put on the template either for markup or code samples
+ */
 handlebars.registerHelper('interpolate-ad-markup', function() {
 	return new handlebars.SafeString(this.adMarkup);
+});
+
+/**
+ * All kinds of style goodness so ad units can be self-contained
+ * Takes a list of styles an turns it into a style attribute
+ */
+handlebars.registerHelper('stylr', function(...styles) {
+	const styleString = styles
+		// we want all but the last meta argument of the helper, hence the slice
+		.slice(0, -1)
+		.map((style) => 
+			Object.keys(style).reduce(
+				(acc, rule) => `${acc}${rule}:${style[rule]};`,
+				''
+			)
+		)
+		.join('');
+	return new handlebars.SafeString(`style="${styleString}"`);
 });
 
 const items = adGallery.map((item) => {
 	const templateFile = fs.readFileSync(`./src/templates/${item.template}.hbs`, 'utf-8')
 	const template = handlebars.compile(templateFile);
+	const markupData = Object.assign({}, sampleAd, { style });
 
 	return Object.assign({}, item, {
-		adMarkup: template(sampleAd)
+		adMarkup: template(markupData)
 	});
 });
 
---
+++ src/style.js
@@ -0,0 +1,83 @@
+/**
+ * I probably could have written a css parser, but instead this:
+ */
+module.exports = {
+	'ad': {
+		root: {
+			display: 'block'
+		},
+		stacked: {
+			'max-width': '200px',
+		},
+		imgOverlay: {
+			position: 'relative',
+		},
+	},
+	'headline': {
+		'font-weight': 'bold',
+		'font-size': '1.3em'
+	},
+	'sponsorWrapper': {
+		'font-size': '0.8em'
+	},
+	'sponsorLabel': {
+		'font-weight': 'bold'
+	},
+	'cta': {
+		'text-decoration': 'none',
+		padding: '3px',
+		color: '#000',
+		background: '#fff',
+		border: '1px solid #000',
+		'font-size': '1.2em',
+	},
+	'body': {
+		margin: '5px 0'
+	},
+
+	/* vanilla left-floated image */
+
+	'imgLeft': {
+		'img': {
+			float: 'left',
+			'margin-right': '5px',
+		}
+	},
+
+	/* overlayed image styles */
+
+	'overlayContent': {
+		root: {
+			position: 'absolute',
+			color: '#fff',
+			width: '320px',
+			top: 0,
+			bottom: 0
+		},
+		'logo': {
+			position: 'absolute',
+			bottom: '5px',
+			right: '3px',
+		},
+		'headline': {
+			position: 'absolute',
+			'text-align': 'center',
+			top: '13px',
+			left: 0,
+			right: 0,
+		},
+		'sponsorWrapper': {
+			position: 'absolute',
+			bottom: '5px',
+			left: '3px',
+		},
+		'cta': {
+			position: 'absolute',
+			bottom: '25%',
+			left: '60px',
+			right: '60px',
+			border: 0,
+			'text-align': 'center',
+		}
+	}, 
+}
